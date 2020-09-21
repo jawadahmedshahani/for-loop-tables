fd7e3b6f50953f70a2239a5c2aeea567d809a150
jawadahmedshahani <70814895+jawadahmedshahani@users.noreply.github.com>
2020-09-21 00:09:55 -0700

    Add files via upload

---
+++ most biggest program on table which I ever understand.cpp
@@ -0,0 +1,64 @@
+
+#include<iostream>
+using namespace std;
+int main()
+
+//int _tmain(int argc, _TCHAR* argv[])
+{
+	int i;
+	int num;
+	
+	int number;
+int ownchoice;
+
+	do
+	{
+			cout<<"enter number from 1 to any number for any number of table:"<<endl;
+			cin>>number;
+		   cout<<"enter num for multiple:"<<endl;
+	    	cin>>num;
+	    	i=1;
+	
+	
+
+	
+	    
+	   
+	    
+					
+	//while( i<=num )
+	
+    
+
+/*	if(num>=101)
+	{
+    	break;
+	}
+	if(number>=100)
+	
+	{
+		break;
+			
+		}   */
+		
+		 for(i=1;i<=num;i++) 
+		 
+		 
+	
+		cout<<number<<" X"<<i<<"="<<number*i<<endl;
+
+			//i=i+1;
+		
+	
+
+cout<<"ownchoice="<<endl;
+cin>>ownchoice;
+	}
+
+	while(ownchoice!=number);
+	
+
+
+	return 0;
+}
+
